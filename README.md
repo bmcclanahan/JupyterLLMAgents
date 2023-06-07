f3326bb953de96305d1b268421da38519cd1e07f
Brian McClanahan <mcclanahanbrian@gmail.com>
2023-06-07 00:08:32 -0400

    enhance functionality

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # JupyterLLMAgents
 A collection of Jupyter notebooks that explore utilizing LLM (large language model) agents for interactive analysis.
+This project includes a chat agent tool which can be accessed as a Jupyter Magic command `%%chat_agent`. By placing the `%%chat_agent` command at the top of a cell you can instruct a LLM to execute different actions and store the result in your notebook environment (e.g. performing analysis on a Pandas Dataframe). Here's an example.
+```
+%%chat_agent -n
+Retrieve the price of Bitcoin and store it in a variable named btc_pr
+```
+By executing a cell that uses the `chat_agent` magic command you can retrieve the current price of Bitcoin and have it stored in a variable named `btc_pr`. The `get_result` function from the `chat_agent` module can be used to retrieve variables produced by the chat agent.
+
+```
+chat_agent.get_result('btc_pr')
+```
+These variables can then be accessed by the chat agent for subsequent references and computations.
+
+Reference the **HelloAgent** notebook for a complete example.
 
 ### Requirements
 This project assumes the use of python 3.9 but may work with other python versions as well. A [OpenAI API key](https://platform.openai.com/account/api-keys) is required and should be set in an environment variable named `OPENAI_API_KEY`.
+
+The **HelloAgent** notebook also requires you to have an [API key](https://serper.dev/) for the Google Search API. The `SERPER_API_KEY` environment variable should be set with this key. 
 
 ### Installation
 Dependencies are managed with [pipenv](https://pipenv.pypa.io/en/latest/). You can install pipenv by running
--- chat_agent.py
+++ chat_agent.py
@@ -1,8 +1,11 @@
 from IPython.core.magic import magics_class, register_line_cell_magic, Magics
 
 import ast
+import argparse
 import astor
 import pandas as pd
+import re
+import os
 
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
@@ -11,6 +14,7 @@ from langchain.llms import OpenAI
 from langchain.tools import Tool
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import AgentAction
+from langchain.utilities import GoogleSerperAPIWrapper
 
 
 from typing import Any, Optional, Dict, List
@@ -22,15 +26,17 @@ class ChatAgentCallbackHandler(BaseCallbackHandler):
 
     def __init__(self):
         self.descriptions = []
+        self.agent_action = None
 
     def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
         """Run on agent action."""
+        print('on action')
         index = action.log.find("Action:")
+        self.agent_action = action
         if index != -1:
             self.descriptions.append(
                  action.log[:index].strip()
             )
-            print('description extracted', self.descriptions[-1])
                   
     
 
@@ -41,11 +47,15 @@ class ChatAgentMagics(Magics):
         #super(ChatAgentMagics, self).__init__(shell)  uncomment this when making this a proper jupyter extension loaded with %load_ext
         self.__agent_input = {}
         self.__llm = OpenAI(temperature=0)
-        self.__tools = [
+        tools = load_tools(
+            ["google-serper"],
+            llm=self.__llm
+        ) if 'SERPER_API_KEY' in os.environ else []
+        self.__tools = tools + [
             Tool.from_function(
-                func=self.perform_dataframe_analysis,
-                name = 'dataframeAnalysis',
-                description='Tool to perform analysis on Pandas dataframes. Input should be python code containing dataframe operations to derive answers to questions. The input code should store the answer in variable named result.'
+                func=self.python_execution,
+                name = 'pythonCodeExecution',
+                description='Tool used to execute Python code. Input should be python code containing statements to derive answers to questions or solutions to instructions. The input code should store the answer in variable named result unless instructed otherwise. The tool may return feedback from the user on the input code. If the result is a numeric value be sure to assign it to a variable with proper formatting without commas, dollar signs,  percent symbols or any other symbol.'
             )
         ]
         self.__agent  = initialize_agent(
@@ -55,7 +65,9 @@ class ChatAgentMagics(Magics):
             max_iterations=3
         )
         self.__callback_handler = ChatAgentCallbackHandler()
-        self.__result_num = 1
+        self.__noninteractive = False
+        self.__verbose = False
+        self.__last_key = None
 
     def is_df_overwrite(self, node: ast.stmt) -> str:
 
@@ -116,59 +128,124 @@ class ChatAgentMagics(Magics):
     def get_agent_input(self):
         return self.__agent_input
 
-    def perform_dataframe_analysis(self, analysis_code: str):
-        analysis_code = self.clean_code(analysis_code)
-        print()
-        print('cleaned code')
-        print(analysis_code)
-        input_environment = {
-            key: self.__agent_input[key]['value']
-            for key in self.__agent_input
-        }
-        environment = {
-            **input_environment,
-            "__builtins__": {
-                **{
-                    builtin: __builtins__[builtin]
-                    for builtin in WHITELISTED_BUILTINS
+    def python_execution(self, analysis_code: str):
+        last_character = self.__callback_handler.agent_action.log.strip()[-1]
+        if last_character == '"' and not analysis_code.endswith('"'):
+            analysis_code += '"' # replace missing quotes that langchain strips
+        try:
+            analysis_code = self.clean_code(analysis_code)
+            print()
+            if self.__verbose:
+                print('input code')
+                print(analysis_code)
+            user_feedback = ''
+            if not self.__noninteractive:
+                prompt = f'''
+    
+    The change agent would like to run the following code:
+    
+    --------------------------------------------------------
+    {analysis_code}
+    --------------------------------------------------------
+    
+    To allow execution type Y or type N to disallow.
+    You may give additional feedback to the option for either option by placing a dash after the allow option followed by the feedbak. For example:
+    Y - this code answers my original question
+    or
+    N - this code does not produce the right answer
+    
+                '''
+                feedback_retrieved = False
+                while not feedback_retrieved:
+                    try:
+                        user_input = input(prompt)
+                        user_input = user_input.strip().split('-')
+                        first_input = user_input[0].strip().lower()
+                        if first_input not in ('y', 'n'):
+                            raise ValueError('Must enter Y or N')
+                        if len(user_input) > 1:
+                            user_feedback = ' - '.join(user_input[1:])
+                        if first_input == 'n':
+                            response_end = 'most likely because it doesn\'t achieve the desired result.' if len(user_feedback) == 0 else f' and has the following feedback: {user_feedback}'
+                            return f'The user disallowed execution of the code{response_end}'
+                        feedback_retrieved = True
+                    except ValueError as e:
+                        print(e)
+                        pass
+                    
+            input_environment = {
+                key: self.__agent_input[key]['value']
+                for key in self.__agent_input
+            }
+            environment = {
+                **input_environment,
+                "__builtins__": {
+                    **{
+                        builtin: __builtins__[builtin]
+                        for builtin in WHITELISTED_BUILTINS
+                    },
                 },
-            },
-        }
-        exec(analysis_code, environment)
-        last_line = analysis_code.split('\n')[-1].strip()
-        if 'result' in last_line and 'result' in environment:
-            result = environment['result']
-        else:
-            result = eval(last_line, environment)
-        result_num = len([key for key in self.__agent_input if 'result' in key])
-        result_key = f'result{self.__result_num}'
-        description = f'object of type {type(result)} related to the thought "{self.__callback_handler.descriptions[-1]}"'
-        if type(result) == pd.DataFrame:
-            description+= f'. The dataframe has the columns {result.columns.values}'
-        self.__agent_input[result_key] = {
-            'value': result, 'description': description
-        }
-        self.__result_num += 1
-        return f'Answer has been successfully derived. Key: {result_key}' if not type(result) == str else result
+            }
+
+            exec(analysis_code, environment)
+
+            code_parse = ast.parse(analysis_code, mode='exec')
+            key_val = 'result'
+            if 'result' in environment:
+                result = environment['result']
+            elif type(code_parse.body[-1]) == ast.Assign:
+                if self.__verbose:
+                    print('The variable `result` was not found in executing environment. Using the assignment on the last code line instead for the result.')
+                key_val = code_parse.body[-1].targets[0].id
+                result = environment[key_val]
+            else:
+                return 'complete. No assignment operation found in last lines of code.'
+            result_num = len([key for key in self.__agent_input if key_val in key])
+            result_key = key_val + (str(result_num + 1) if result_num > 0 else '')
+            alias_description = f'It is an alias for {key_val} and' if key_val else ''
+            description = f'object of type {type(result)} related to the thought "{self.__callback_handler.descriptions[-1]}"'
+            if type(result) == pd.DataFrame:
+                description+= f'. The dataframe has the columns {result.columns.values}'
+            self.__agent_input[result_key] = {
+                'value': result, 'description': description
+            }
+            response_end = '' if len(user_feedback) == 0 else f' - The user has the following feedback: {user_feedback}'
+            self.__last_key = result_key
+            return f'Answer has been successfully derived. Key: {result_key}{response_end}' if not type(result) == str else result + response_end
+        except Exception as e:
+            return f'execution failed with the error message: {str(e)}'
 
     def chat_agent(self, line: Optional[str], cell: Optional[str]=None):
-        "Magic that works both as %lcmagic and as %%lcmagic"
-        if cell is None:
-            response = agent.run(line)
-        else:
-            available_variables = '\n'.join(
-                [
-                    key + ' - ' + self.__agent_input[key]['description']
-                    for key in self.__agent_input
-                ]
-            )
-            cell = cell + """\nWhen using the dataframeAnalysis tool you may assume that you have access to the following variables when writing the code:
-            """ + available_variables
-            print('prompt is ')
-            print(cell)
-            response = self.__agent.run(cell, callbacks=[self.__callback_handler])
-        result_key = result_key = f'result{self.__result_num -1}'
-        return self.__agent_input[result_key]['value'] if result_key in self.__agent_input else response
+        "Magic that works as %%chat_agent"
+        options = list(filter(lambda x: len(x) != 0, line.strip().split(' ')))
+        parser = argparse.ArgumentParser(description='chat agent options')
+        parser.add_argument(
+            '--noninteractive', '-n', action='store_true',
+            help='runs the agent in a non interactive mode where the user is not prompted for input',
+            default=False, required=False
+        )
+        parser.add_argument(
+            '--verbose', '-v', action='store_true',
+            help='verbose option',
+            default=False, required=False
+        )
+        args = parser.parse_args(options)
+        self.__noninteractive = args.noninteractive
+        self.__verbose = args.verbose
+        available_variables = '\n\n'.join(
+            [
+                key + ' - ' + self.__agent_input[key]['description']
+                for key in self.__agent_input
+            ]
+        )
+        cell = cell + ('''\nWhen using the pythonCodeExecution tool you may assume that you have access to the following variables when writing the code:
+
+''' if len(self.__agent_input) > 0 else '') + available_variables
+        cell = cell.strip()
+        print('Prompt:')
+        print(cell)
+        response = self.__agent.run(cell, callbacks=[self.__callback_handler])
+        return response
 
 
 chat_agent_magic = ChatAgentMagics()
